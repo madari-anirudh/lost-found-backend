e3006456cb30c9a8e7f55dd3c6954315e91369ed
madari anirudh <anirudhmahendra2006@gmail.com>
2026-05-07 13:37:16 +0530

    Hard code URl commenting

--- backend/android-app/admin-panel/src/api/api.js
+++ backend/android-app/admin-panel/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://lost-found-api-q597.onrender.com/api"
+  baseURL: "https://lost-found-api-q597.onrender.com/api"  //  hard code URl 
 });
 
 API.interceptors.request.use((req) => {
