3ac289bbb90ebbd9aba0c334f99863a4493fa99b
madari anirudh <anirudhmahendra2006@gmail.com>
2026-05-07 13:34:24 +0530

    adding files

--- backend/android-app/admin-panel/src/api/api.js
+++ backend/android-app/admin-panel/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://lost-found-api-q597.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
